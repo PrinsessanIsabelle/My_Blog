3cda1c2810e587739acbf228248ca902311223ee
Isabelle Lindberg <h24islin@du.se>
2026-03-05 16:52:26 +0100

    first commit

---
+++ server/models/post.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    'post',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      imageURL: {
+        type: DataTypes.STRING(255),
+        allowNull: true,
+      },
+    },
+    { underscored: true },
+  );
+};
