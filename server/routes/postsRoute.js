07624fb4eee762e5ee69d1b9ef5b35768c2b919d
Isabelle Lindberg <h24islin@du.se>
2026-03-05 17:51:32 +0100

    Add initial Postman resources and implement CRUD operations for posts

--- server/routes/postsRoute.js
+++ server/routes/postsRoute.js
@@ -1,6 +1,39 @@
 const router = require('express').Router();
+const db = require('../models/');
 
 router.get('/', (req, res) => {
-    res.send('Get posts');
+  db.post.findAll().then((result) => {
+    res.send(result);
+  })
+    // res.send('Get posts');
 });
+
+router.post ('/', (req, res) => {
+   db.post.create(req.body).then((result) => {
+    res.send(result);
+  })
+    // res.send(req.body);
+});
+
+router.put('/', (req, res) => {
+  db.post.update(req.body, {
+    where: {
+      id: req.body.id
+    }
+  }).then((result) => {
+    res.send(result);
+  });
+  // res.send('Put posts');
+});
+
+router.delete('/', (req, res) => {
+ db.post.destroy({ where: {
+    id: req.body.id
+  }}).then(() => {
+    res.json('Inlägget raderades');
+  });
+    // res.send('Delete post');
+});
+
+
 module.exports = router;
